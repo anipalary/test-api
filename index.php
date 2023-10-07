3543e04ed6bdda18017be82105e26ab30ab9d75b
anipalary <60285472+anipalary@users.noreply.github.com>
2023-10-07 16:02:28 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+1.1. GET /api/v1/notebook/<br>
+1.2. POST /api/v1/notebook/<br>
+1.3. GET /api/v1/notebook/&ltid&gt/<br>
+1.4. POST /api/v1/notebook/&ltid&gt/<br>
+1.5. DELETE /api/v1/notebook/&ltid&gt/<br>
+<br>
+1. ФИО (обязательное)<br>
+2. Компания<br>
+3. Телефон (обязательное)<br>
+4. Email (обязательное)<br>
+5. Дата рождения <br>
+6. Фото
